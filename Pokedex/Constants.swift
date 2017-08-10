2ef8464ee792e3c20102f4216a8156c43485a82d
David Sternheim <sterndav14@gmail.com>
2017-08-09 22:12:34 -0400

    Added api calling

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by David Sternheim on 8/9/17.
+//  Copyright © 2017 David Sternheim. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
